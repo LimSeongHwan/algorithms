e1b1ac2ef2ce1d846cea5d55b1635dc0947b81dc
LSH <27benecia@naver.com>
2021-02-09 00:24:29 +0900

    BJ_1037 and BJ_1059 upload

---
+++ BJ_1037.py
@@ -0,0 +1,7 @@
+def solution(case):
+    divisors = list(map(int, input().split()))
+    divisors.sort()
+    return divisors[0] * divisors[-1]
+
+case = int(input())
+print(solution(case))
---
+++ BJ_1059.py
@@ -0,0 +1,32 @@
+def solution(n):
+
+    numbers = list(map(int, input().split()))
+    numbers.sort()
+    target = int(input())
+    idx = 0
+    res = []
+
+    for i in range(len(numbers)):
+        if numbers[i] == target:
+            return 0
+        elif numbers[i] > target:
+            idx = i
+            break
+
+    if idx == 0:
+        for i in range(1, numbers[idx]):
+            for j in range(i, numbers[idx]):
+                if (target >= i) and (target <= j) and i != j:
+                    res.append([i, j])
+
+        return len(res)
+
+    for i in range(numbers[idx-1] + 1, numbers[idx]):
+        for j in range(i, numbers[idx]):
+            if (target >= i) and (target <= j) and i != j:
+                res.append([i, j])
+
+    return len(res)
+
+n = int(input())
+print(solution(n))
