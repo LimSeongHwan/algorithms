55620e8b32a8ddf33ae3677153a9c9746e66f285
LSH <27benecia@naver.com>
2021-06-17 19:51:51 +0900

    Upload

--- BJ_16918.py
+++ BJ_16918.py
@@ -1,47 +1,49 @@
 def find_bomb():
     bomb_idx = []
+    not_bomb_idx = []
     for i in range(r):
         for j in range(c):
             if area[i][j] == 'O':
                 bomb_idx.append((i, j))
-    return bomb_idx
+            else:
+                not_bomb_idx.append((i, j))
+    return bomb_idx, not_bomb_idx
 
 
-def plant_bomb():
-    for i in range(r):
-        for j in range(c):
-            if area[i][j] == '.':
-                area[i][j] = 'O'
+def plant_bomb(not_bomb_idx):
+    global n
+    if n == 0:
+        return False
+    for y, x in not_bomb_idx:
+        area[y][x] = 'O'
+    n -= 1
+    return True
 
 
 def bomb(bomb_idx):
+    global n
+    dy = [-1, 1, 0, 0]
+    dx = [0, 0, -1, 1]
+    if n == 0:
+        return
     for y, x in bomb_idx:
         area[y][x] = '.'
         for i in range(4):
             ny = dy[i] + y
             nx = dx[i] + x
-
             if (0 <= ny < r) and (0 <= nx < c) and (area[ny][nx] == 'O'):
                 area[ny][nx] = '.'
-            
+    n -= 1
+
 
 r, c, n = map(int, input().split())
 area = [list(input()) for _ in range(r)]
-bomb_idx = []
-dy = [-1, 1, 0, 0]
-dx = [0, 0, -1, 1]
 n -= 1
 
-while True:
-    if n != 0:
-        bomb_idx = find_bomb()
-    if n != 0:
-        plant_bomb()
-        n -= 1
-    if n != 0:
+while n != 0:
+    bomb_idx, not_bomb_idx = find_bomb()
+    if plant_bomb(not_bomb_idx):
         bomb(bomb_idx)
-        n -= 1
-    if n == 0:
-        for value in area:
-            print(''.join(value))
-        break
+
+for value in area:
+    print(''.join(value))
